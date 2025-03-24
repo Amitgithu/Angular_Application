f45d8bfb90984b9cdfffd083d589496292c861f2
Amit Kumar <amit909563@gmail.com>
2025-03-24 11:15:16 +0530

    Added: Updating User name on Real-Time in Hello Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  title = 'MyAngular';
+  imageURL = '../assets/BL_logo_square_jpg.jpg';
+  url = 'https://www.bridgelabz.com/';
+  username: String = '';
+
+  ngOnInit(): void {
+    console.log('Hello from ngOnInit');
+    this.title = 'Hello from BridgeLabz.';
+  }
+
+  onClick($event: MouseEvent) {
+    console.log('Button was clicked!', $event);
+    window.open(this.url, '_blank');
+  }
 }
