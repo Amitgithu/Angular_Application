54fee42e45da2d2d21a74e99edb1ac5729b6ab02
Amit Kumar <amit909563@gmail.com>
2025-03-24 11:20:38 +0530

    Added: Displaying User Name in Hello Message with Error-checking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  title = 'MyAngular';
+  imageURL = '../assets/BL_logo_square_jpg.jpg';
+  url = 'https://www.bridgelabz.com/';
+  userName: string = "";
+  nameError = "";
+
+  ngOnInit(): void {
+    console.log('Hello from ngOnInit');
+    this.title = 'Hello from BridgeLabz.';
+  }
+
+  onClick($event: MouseEvent) {
+    console.log('Button was clicked!', $event);
+    window.open(this.url, '_blank');
+  }
+
+  onInput($event: Event) {
+    console.log('Input event fired! Changes have been made by User.', $event);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
 }
