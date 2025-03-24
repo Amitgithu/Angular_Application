308bc9b8f75039106fef8bac6e473d3e2e97de1d
Amit Kumar <amit909563@gmail.com>
2025-03-24 10:59:06 +0530

    Added: Displaying Message Using Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-angular-app';
+  title = 'MyAngular';
+  imageURL = '../assets/BL_logo_square_jpg.jpg';
+
+  ngOnInit(): void {
+    console.log('Hello from ngOnInit');
+    this.title = 'Hello from BridgeLabz.';
+  }
 }
